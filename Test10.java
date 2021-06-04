5c49db508cf4a9147bd492179120663d2890c64b
rekesai <546446335@qq.com>
2021-06-04 23:59:33 +0800

    11

--- Test10.java
+++ Test10.java
@@ -1,4 +1,81 @@
+import java.util.Scanner;
+
 public class Test10 {
+
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+        int count = 0;
+        if(str.length() <= 4){
+            count += 5;
+        }
+        else if(str.length() <= 7){
+            count += 10;
+        }
+        else{
+            count += 25;
+        }
+        int flagNum = 0;
+        int flagE = 0;
+        int flage = 0;
+        int flagSymbol = 0;
+        for(int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+            if(ch >= 48 && ch <= 57){
+                flagNum++;
+            }
+            if(ch >= 65 && ch <= 90){
+                flagE++;
+            }
+            if(ch >= 97 && ch <= 122){
+                flage++;
+            }
+            if(ch >= 0x21 && ch <= 0x2F || ch >= 0x3A && ch <= 0x40
+                    || ch >= 0x5B && ch <= 0x60 || ch >= 0x7B && ch <= 0x7E){
+                flagSymbol++;
+            }
+        }
+        if(flagNum == 1){
+            count += 10;
+        }
+        else if(flagNum > 1){
+            count += 20;
+        }
+        if(flagE == 0 && flage != 0 || flagE != 0 && flage == 0){
+            count +=10;
+        }
+        else if(flagE != 0 && flage != 0){
+            count += 20;
+        }
+        if(flagSymbol == 1){
+            count += 10;
+        }
+        else if(flagSymbol > 1){
+            count += 25;
+        }
+        if(count >= 90){
+            System.out.println("VERY_SECURE");
+        }
+        else if(count >= 80){
+            System.out.println("SECURE");
+        }
+        else if(count >= 70){
+            System.out.println("VERY_STRONG");
+        }
+        else if(count >= 60){
+            System.out.println("SECURE");
+        }
+        else if(count >= 50){
+            System.out.println("AVERAGE");
+        }
+        else if(count >= 25){
+            System.out.println("WEAK");
+        }
+        else{
+            System.out.println("VERY_WEAK");
+        }
+    }
+
 
     //求高效算法得出玩井字棋的玩家是否获胜
     public boolean checkWon(int[][] board) {
---
+++ Test11.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Test11 {
+
+    //输入一个byte型数字，求其二进制数字中1的最大连续数，如输入3，输出2
+    public static void main(String[] args) throws Exception{
+        Scanner s = new Scanner(System.in);
+        byte n = s.nextByte();
+        int i = n;
+        int count = 0;
+        int max = 0;
+        while(i != 0){
+            int tmp = i & 1;
+            if(tmp == 1){
+                count++;
+            }
+            else{
+                count = 0;
+            }
+            max = Math.max(max, count);
+            i = i >>> 1;
+        }
+        System.out.println(max);
+    }
+
+
+
+    //有一个无穷大的满二叉树，从根节点开始编号为1，从上至下，从左至右编号
+    //现在输入两个数字a  b  代表两个节点编号，求出他们的最小的公共祖先节点编号
+    public int getLCA(int a, int b) {
+        //既然是求最近的公共祖先节点，那就让他们谁大谁就除2，就能得出谁的父节点
+        //如此循环，当两个数字求到的父节点一样时就得出了最小的公共祖先节点，不放画图试试看
+        while(a != b){
+            if(a > b){
+                a = a/2;
+            }
+            else{
+                b = b/2;
+            }
+        }
+        return a;
+    }
+
+
+}
